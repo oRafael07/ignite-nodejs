4653e9490a3576f087d2ad85c39f35f248375b29
Rafael Rocha <rafadeveloper.contato@gmail.com>
2022-11-22 08:59:13 -0400

    init

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from 'express'
+import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
+
+const categoriesRoutes = Router()
+
+const categoriesRepository = new CategoriesRepository()
+
+categoriesRoutes.post("/", (req: Request, res: Response) => {
+  const { name, description } = req.body
+
+  const category = new Category()
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date()
+  })
+
+  categoriesRepository.create(category)
+
+  return res.status(201).json(category)
+})
+
+export { categoriesRoutes }
